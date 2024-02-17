ed80cfdb65e74351465e34bc3cc6b74dd1ba88e2
JonasPapinigis <111145459+JonasPapinigis@users.noreply.github.com>
2024-02-17 21:00:26 +0000

    Camera

--- Fox Game/Assets/Scripts/CameraControler.cs
+++ Fox Game/Assets/Scripts/CameraControler.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class CameraControler : MonoBehaviour
 {
     public float FollowSpeed = 100f;
-    public float yOffset = 1f;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
     public Transform target;
-    // Update is called once per frame
+
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x,target.position.y,-10f);
+        
+        Vector3 newPos = new Vector3(target.position.x+xOffset,target.position.y+yOffset,zOffset);
         transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
 
     }
