4e078ebf341bd94339b71a2551f638e84ed9a9f0
Owen <owenjgibson@gmail.com>
2024-02-17 18:21:14 +0000

    Jump and floor added

--- Fox Game/Assets/Scripts/FoxController.cs
+++ Fox Game/Assets/Scripts/FoxController.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class FoxController : MonoBehaviour
 {
-    private float walkSpeed = 5f;
-    private float jumpSpeed = 10f;
+    private float walkSpeed = 20f;
+    private float jumpSpeed = 80f;
     private float crouchMultiplier = 2f;
-    private bool isGrounded;
+    private bool isGrounded = true;
     private bool isCrouching;
     private Rigidbody2D rb;
+    private Collider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
+        collider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         HandleWalk();
         HandleJump();
         HandleCrouch();
@@ -28,7 +29,8 @@ public class FoxController : MonoBehaviour
 
     public void HandleJump() 
     {
-        if (InputHandler.Instance.IsJumpPressed()) 
+        CheckGrounded();
+        if (InputHandler.Instance.IsJumpPressed() && isGrounded) 
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed); 
         }
@@ -51,6 +53,10 @@ public class FoxController : MonoBehaviour
     {
         float movementSpeed = InputHandler.Instance.IsWalkPressed() * walkSpeed;
 
-        rb.velocity = new Vector2(rb.velocity.x, movementSpeed);
+        rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+    }
+    private void CheckGrounded() 
+    {
+        isGrounded = collider.IsTouchingLayers(LayerMask.GetMask("Floor"));
     }
 }
