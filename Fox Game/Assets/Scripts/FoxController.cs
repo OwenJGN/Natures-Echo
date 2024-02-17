ed341f339b22ba34b3ba236731839353f29d3415
Owen <owenjgibson@gmail.com>
2024-02-17 20:37:09 +0000

    Corrected the animation

--- Fox Game/Assets/Scripts/FoxController.cs
+++ Fox Game/Assets/Scripts/FoxController.cs
@@ -4,52 +4,64 @@ using UnityEngine;
 
 public class FoxController : MonoBehaviour
 {
-    private float walkSpeed = 120f;
-    private float jumpSpeed = 320f;
-    private float crouchMultiplier = 2f;
+   
+
+    private float walkSpeed = 50f;
+    private float jumpSpeed = 200f;
+    private float crouchMultiplier = 0.5f; // Assuming this reduces speed for crouching
     private bool isGrounded = true;
     private bool isCrouching;
     private Rigidbody2D rb;
     private Collider2D collider;
+    private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        // Explicitly set initial animation states
+        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsJumping", false);
+        animator.SetBool("IsCrouching", false);
     }
 
-    // Update is called once per frame
     void Update()
     {
+
         HandleWalk();
         HandleJump();
         HandleCrouch();
     }
 
-    public void HandleJump() 
+    public void HandleJump()
     {
-        CheckGrounded();
-        if (InputHandler.Instance.IsJumpPressed() && isGrounded) 
+        if (InputHandler.Instance.IsJumpPressed())
+
+            CheckGrounded();
+        if (InputHandler.Instance.IsJumpPressed() && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed); 
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
         }
     }
 
-    public void HandleCrouch() 
+    public void HandleCrouch()
     {
-        if (InputHandler.Instance.IsCrouchPressed())
+        bool isCrouchPressed = InputHandler.Instance.IsCrouchPressed();
+        isCrouching = isCrouchPressed;
+        if (isCrouchPressed)
         {
-            isCrouching = true;
             rb.velocity = new Vector2(rb.velocity.x * crouchMultiplier, rb.velocity.y);
         }
-        else 
-        {
-            isCrouching = false;
-        }
+        animator.SetBool("IsCrouching", isCrouchPressed);
     }
 
-    public void HandleWalk() 
+    public void HandleWalk()
     {
         float horizontalInput = InputHandler.Instance.IsWalkPressed();
         float targetVelocityX = horizontalInput * walkSpeed;
@@ -68,11 +80,21 @@ public class FoxController : MonoBehaviour
             velocityChangeX = Mathf.Clamp(velocityChangeX, -maxVelocityChange, maxVelocityChange);
             rb.velocity = new Vector2(rb.velocity.x + velocityChangeX, rb.velocity.y);
         }
+
+        // Here we ensure that IsWalking is only true if there is significant movement.
+        bool isWalking = Mathf.Abs(walkSpeed) > 0;
+
+        animator.SetBool("IsWalking", isWalking);
     }
 
-
-    private void CheckGrounded() 
+    private void CheckGrounded()
     {
         isGrounded = collider.IsTouchingLayers(LayerMask.GetMask("Floor"));
+        // Since ground check can influence jumping animation, it's good to update it here too.
+        animator.SetBool("IsJumping", !isGrounded && rb.velocity.y != 0);
+    }
+    private bool HandleStanding()
+    {
+        return false;
     }
 }
