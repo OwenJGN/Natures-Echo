6d58f870003c858c5826ed7004c3450a67b686af
Owen <owenjgibson@gmail.com>
2024-02-17 16:14:51 +0000

    class changes

--- Fox Game/Assets/FoxController.cs
+++ Fox Game/Assets/FoxController.cs
@@ -6,32 +6,51 @@ public class FoxController : MonoBehaviour
 {
     private float walkSpeed = 5f;
     private float jumpSpeed = 10f;
-    private float crouchMultipler = 2f;
+    private float crouchMultiplier = 2f;
+    private bool isGrounded;
+    private bool isCrouching;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        HandleWalk();
+        HandleJump();
+        HandleCrouch();
     }
 
-    void JumpAction() 
-    { 
-    
-    }
-
-    void CrouchAction() 
+    public void HandleJump() 
     {
-        
+        if (InputHandler.Instance.IsJumpPressed()) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed); 
+        }
     }
 
-    void WalkAction() 
-    { 
-    
+    public void HandleCrouch() 
+    {
+        if (InputHandler.Instance.IsCrouchPressed())
+        {
+            isCrouching = true;
+            rb.velocity = new Vector2(rb.velocity.x * crouchMultiplier, rb.velocity.y);
+        }
+        else 
+        {
+            isCrouching = false;
+        }
+    }
+
+    public void HandleWalk() 
+    {
+        float movementSpeed = InputHandler.Instance.IsWalkPressed() * walkSpeed;
+
+        rb.velocity = new Vector2(rb.velocity.x, movementSpeed);
     }
 }
